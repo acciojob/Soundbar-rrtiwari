9ca7291367b4f904fd1537d4ce3b471012e03876
Rahul Tiwari <128848653+rrtiwari@users.noreply.github.com>
2025-08-27 16:34:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
-//your JS code here. If required.
+const sounds = [
+  "harmonica1.mp3",
+  "harmonica2.mp3",
+  "harmonica3.mp3",
+  "harmonica4.mp3",
+  "harmonica5.mp3",
+  "harmonica6.mp3"
+];
+
+const buttons = document.querySelectorAll(".btn");
+const stopButton = document.querySelector(".stop");
+let audio = null;
+
+buttons.forEach((button, index) => {
+  button.addEventListener("click", () => {
+    if (audio) {
+      audio.pause();
+    }
+    audio = new Audio(`sounds/${sounds[index]}`);
+    audio.play();
+  });
+});
+
+stopButton.addEventListener("click", () => {
+  if (audio) {
+    audio.pause();
+    audio.currentTime = 0;
+  }
+});
+
